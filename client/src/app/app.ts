e0a3c6a18dcb33f90ee5b65e77599c7eae39b343
jackson <temp@example.com>
2025-10-26 13:56:43 +0800

    refactor: 將 members 屬性改為使用 Angular signal
    - 從 @angular/core 匯入 signal
    - 將 members 屬性從普通變數改為 signal<any>([])
    - 更新 HTTP 回應處理,使用 members.set() 方法
    - 修正模板中的 members 存取,改用 members() 函式呼叫

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +10,11 @@ import { Component, inject, OnInit } from '@angular/core';
 export class App implements OnInit {
   private http = inject(HttpClient);
   protected  title = 'Dating App';
-  protected members: any ;
+  protected members = signal<any>([]);
 
   ngOnInit(): void {
     this.http.get('https://localhost:5001/api/members').subscribe({
-      next: response => this.members=response,
+      next: response => this.members.set(response),
       error: err => console.error(err),
       complete: () => console.log('Request complete')
     });
